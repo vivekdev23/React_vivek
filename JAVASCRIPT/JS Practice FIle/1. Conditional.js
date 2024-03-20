3366de2d4e4cb0bf38e07cfee0a0bbc5dcb2d88f
vivekdev <techievivek2023@gmail.com>
2024-03-20 12:41:19 +0530

    20-03-2024

---
+++ JAVASCRIPT/JS Practice FIle/1. Conditional.js
@@ -0,0 +1,87 @@
+
+
+// 1th Check if a number is positive, negative, or zero
+
+let num = 0;
+
+if (num > 0) {
+    console.log("The number is positive");
+
+} else if (num < 0) {
+    console.log("The number is negative");
+
+} else {
+    console.log("The number is zero");
+}
+
+// 2nd Check if a person is eligible to vote
+
+let age = 21;
+
+if (age >= 18) {
+    console.log("The person is eligible to vote");
+} else {
+    console.log("The person is not eligible to vote");
+}
+
+// 3rd Determine the largest of three numbers
+
+
+let a = 15;
+let b = 10;
+let c = 8;
+
+if (a >= b && a >= c)
+{
+    console.log("The largest number is: " + a);
+} 
+else if (b >= a && b >= c) {
+  
+    console.log("The largest number is: " + b);
+} 
+else {
+  
+    console.log("The largest number is: " + c);
+}
+
+
+// 4th Check if a year is a leap year 
+
+let year = 2004;
+
+ if (year % 4 == 0) {
+   
+        console.log(year + " is a leap year");
+   
+    } else {
+        console.log(year + " is not a leap year");
+    }
+
+
+// 5th  Determine the grade based on a score
+
+
+let score = 85;
+
+let grade;
+
+if (score >= 86) {
+    grade = "A";
+  
+} else if (score >= 75) {
+    grade = "B";
+  
+} else if (score >= 65) {
+    grade = "C";
+  
+} else if (score >= 45) {
+    grade = "D";
+  
+} else {
+    grade = "FAIL";
+}
+
+console.log("The grade for the score " + score + " is: " + grade);
+
+
+
---
+++ JAVASCRIPT/JS Practice FIle/2. Opereters.js
@@ -0,0 +1,56 @@
+// 1 st programm  : Create a program that performs arithmetic operations on two numbers.
+
+let a = 10;
+let b = 5;
+
+console.log("a + b = ", a + b);
+console.log("a - b = ", a - b);
+console.log("a * b = ", a * b);
+console.log("a / b = ", a / b);
+console.log("a % b = ", a % b);
+console.log("a ** b = ", a ** b);
+console.log("a++ = ", a++);
+console.log("a-- = ", a--);
+
+//  2nd Programm : Use compound assignment operators to update variables.
+
+let x = 10;
+console.log("x+=5 is" ,x += 5);
+console.log("x-=5 is" ,x -= 5);
+console.log("x*=5 is" ,x *= 5);
+console.log("x/=5 is" ,x /= 5);
+console.log("x%=5 is" ,x %= 5);
+console.log("x**=5 is" ,x **= 5);
+
+
+// 3rd prograam : Compare two numbers and display the result.
+
+let y = 10;  
+let z = 10;
+
+console.log("y==z is" ,y==z);
+console.log("y!=z is" ,y!=z);
+console.log("y===z is" ,y===z);
+console.log("y!==z is" ,y!==z);
+console.log("y>z is" ,y>z);
+console.log("y<z is" ,y<z);
+console.log("y>=z is" ,y>=z);
+console.log("y<=z is" ,y<=z);
+
+
+// 4th Use the ternary operator to check a condition.
+
+let age = 18;
+let result = (age >= 18) ? "You can vote." : "You cannot vote.";
+console.log(result);
+
+// 5th Compare two strings and display the result.
+
+let str1 = "Hello";
+let str2 = "World";
+console.log(str1 === str2);
+console.log(str1 == str2);
+
+
+
+
